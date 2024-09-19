68164cc51ffc40e037c28ebccc5faca93568857f
heyAnkit18 <ankitkumarjune18@gmail.com>
2024-09-19 11:56:51 +0530

    Navbar Added

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import { Link } from 'react-scroll';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import './Navbar.css';
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => setIsOpen(!isOpen);
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <div className="navbar-logo">
+          <h1>AK</h1>
+        </div>
+        <div className="navbar-toggle" onClick={toggleMenu}>
+          {isOpen ? <FaTimes className="toggle-icon" /> : <FaBars className="toggle-icon" />}
+        </div>
+        <ul className={`navbar-menu ${isOpen ? 'active' : ''}`}>
+          <li><Link to="hero" smooth={true} duration={500} onClick={() => setIsOpen(false)}>Home</Link></li>
+          <li><Link to="about" smooth={true} duration={500} onClick={() => setIsOpen(false)}>About</Link></li>
+          <li><Link to="skills" smooth={true} duration={500} onClick={() => setIsOpen(false)}>Skills</Link></li>
+          <li><Link to="projects" smooth={true} duration={500} onClick={() => setIsOpen(false)}>Projects</Link></li>
+          <li><Link to="contact" smooth={true} duration={500} onClick={() => setIsOpen(false)}>Contact</Link></li>
+          <li><Link to="footer" smooth={true} duration={500} onClick={() => setIsOpen(false)}>Footer</Link></li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
